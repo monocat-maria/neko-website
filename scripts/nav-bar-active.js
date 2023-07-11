38acf4a250a47d3f76f645a27460534184d0808a
monocat-neko <monocat.lab@outlook.com>
2023-07-11 16:00:31 -0300

    Added nav-active script

---
+++ scripts/nav-bar-active.js
@@ -0,0 +1,10 @@
+ let list = document.querySelectorAll('.nav li'); //variable element list that selects the var document with the method querySelect to select the html code 
+ 
+ function active(){ //function called active 
+    list.forEach((i) =>
+    i.classList.remove('active')); // allows manipulatio of element's class content in this case on the html .nav li.active
+    this.classList.add('active');
+ }
+
+ list.forEach((i) =>
+ i.addEventListener('click',active)); //by clicking, callback the function active
